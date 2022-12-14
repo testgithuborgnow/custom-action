dbfea2e9d1226d221353c168c8576b67aef208a0
mahesh.ganji <mahesh.ganji@servicenow.com>
2022-12-14 10:37:57 +0530

    testing

--- src/main.js
+++ src/main.js
@@ -62,7 +62,16 @@ const main = async() => {
                 'Authorization': 'Basic ' + `${encodedToken}`
             };
             let httpHeaders = { headers: defaultHeaders };
-            response = await axios.get(restendpoint, httpHeaders);
+            //response = await axios.get(restendpoint, httpHeaders);
+            response = axios.get(restendpoint, httpHeaders).then(
+                (response) => {
+                    console.log(response);
+                  }, (error) => {
+                    console.log(error);
+                  }
+            );
+            //console.log("response => "+response+", Stringified response => "+JSON.stringify(response));
+            console.log(JSON.stringify(response));
             console.log("response => "+response+",  Stringified response => "+JSON.stringify(response));
         } catch (err) {
             if (!err.response) {
