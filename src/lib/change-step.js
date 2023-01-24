9fd45d63da8a88fe3436320fc8b69c11aac4a6c9
mahesh.ganji <mahesh.ganji@servicenow.com>
2023-01-24 16:54:05 +0530

    testing

--- src/lib/change-step.js
+++ src/lib/change-step.js
@@ -7,42 +7,21 @@ async function createStep({
     payload,
     httpHeaders
   }){
-
+    
+    var response;
     let options = {
         method: "POST",
         headers: httpHeaders,
         body: payload
       };
+     try{
+      response = await axios.post(postendpoint, JSON.stringify(payload), httpHeaders);
+     }
+     catch(err){
+       console.log(err);
+     }
 
-
-    let retryCount = 0;
-    let overallTimerId;
-    
-      // make the API call
-      fetch(postendpoint, options)
-        .then(response => {
-          // process the response
-          console.log(response);
-          console.log(JSON.stringify(response));
-          clearTimeout(overallTimerId);
-        }).catch(error => {
-            console.log(error);
-            if (retryCount < 3) {
-              retryCount++;
-              console.log("Retrying API call: ", retryCount);
-              setTimeout(() => makeApiCall(), 3000);
-            } else {
-              console.log("Retry limit reached, stopping API call");
-              clearTimeout(overallTimerId);
-            }
-          });
-          
-          overallTimerId = setTimeout(() => {
-            console.log("Overall time limit reached, stopping API call");
-          }, 15000);
-
-    return true;
-
+    return response;
 
   }
   module.exports = {createStep};
