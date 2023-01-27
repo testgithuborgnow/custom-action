518cf4b0584fccdbe507952a80bc30820dcbcfcc
mahesh.ganji <mahesh.ganji@servicenow.com>
2023-01-27 11:19:54 +0530

    tst

--- src/lib/create-change.js
+++ src/lib/create-change.js
@@ -2,24 +2,24 @@ const core = require('@actions/core');
 const axios = require('axios');
 
 async function createChange({
-  instanceUrl,
-  toolId,
-  username,
-  passwd,
-  jobname,
-  githubContextStr,
-  changeRequestDetailsStr,
-  changeCreationTimeOut,
-  abortOnChangeCreationFailure
+    instanceUrl,
+    toolId,
+    username,
+    passwd,
+    jobname,
+    githubContextStr,
+    changeRequestDetailsStr,
+    changeCreationTimeOut,
+    abortOnChangeCreationFailure
 }) {
-   
+
     console.log('Calling Change Control API to create change....');
-    
+
     let changeRequestDetails;
     let attempts = 0;
 
     try {
-      changeRequestDetails = JSON.parse(changeRequestDetailsStr);
+        changeRequestDetails = JSON.parse(changeRequestDetailsStr);
     } catch (e) {
         console.log(`Error occured with message ${e}`);
         throw new Error("Failed parsing changeRequestDetails");
@@ -35,7 +35,7 @@ async function createChange({
     }
 
     let payload;
-    
+
     try {
         payload = {
             'toolId': toolId,
@@ -59,79 +59,90 @@ async function createChange({
     let status = false;
 
     while (attempts < 3) {
+        // try {
+        ++attempts;
+        const token = `${username}:${passwd}`;
+        const encodedToken = Buffer.from(token).toString('base64');
+
+        const defaultHeaders = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Basic ' + `${encodedToken}`
+        };
+        let httpHeaders = { headers: defaultHeaders };
+        let apiError = null;
         try {
-            ++attempts;
-            const token = `${username}:${passwd}`;
-            const encodedToken = Buffer.from(token).toString('base64');
-
-            const defaultHeaders = {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': 'Basic ' + `${encodedToken}`
-            };
-            let httpHeaders = { headers: defaultHeaders };
-            response = await axios.post(postendpoint, JSON.stringify(payload), httpHeaders);
-
-            await new Promise((resolve, reject) => {
-                setTimeout(() => {
-                  if (apiError) {
-                    reject(apiError);
-                  } else {
+            const response = await axios.post(postendpoint, JSON.stringify(payload), httpHeaders);
+            const endTime = new Date();
+            const timeTaken = endTime - startTime;
+            console.log(`API call took ${timeTaken}ms`);
+            return response;
+          } catch (error) {
+            apiError = error;
+          }
+        new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (apiError) {
+                    reject(err);
+                } else {
                     console.log(`API call took ${timeout}ms`);
                     resolve();
-                  }
-                }, 1000*10);
-              });
-
-            status = true;
-            break;
-        } catch (err) {
-            if (err.message.includes('ECONNREFUSED') || err.message.includes('ENOTFOUND')) {
-                throw new Error('Invalid ServiceNow Instance URL. Please correct the URL and try again.');
-            }
-            
-            if (err.message.includes('401')) {
-                throw new Error('Invalid Credentials. Please correct the credentials and try again.');
-            }
-               
-            if (err.message.includes('405')) {
-                throw new Error('Response Code from ServiceNow is 405. Please correct ServiceNow logs for more details.');
-            }
-
-            if (!err.response) {
-                throw new Error('No response from ServiceNow. Please check ServiceNow logs for more details.');
-            }
-
-            if (err.response.status == 500) {
-                throw new Error('Response Code from ServiceNow is 500. Please check ServiceNow logs for more details.')
-            }
-            
-            if (err.response.status == 400) {
-                let errMsg = 'ServiceNow DevOps Change is not created. Please check ServiceNow logs for more details.';
-                let responseData = err.response.data;
-                if (responseData && responseData.error && responseData.error.message) {
-                    errMsg = responseData.error.message;
-                } else if (responseData && responseData.result && responseData.result.details && responseData.result.details.errors) {
-                    errMsg = 'ServiceNow DevOps Change is not created. ';
-                    let errors = err.response.data.result.details.errors;
-                    for (var index in errors) {
-                        errMsg = errMsg + errors[index].message;
-                    }
                 }
-                if (errMsg.indexOf('callbackURL') == -1)
-                    throw new Error(errMsg);
-                else if (attempts >= 3) {
-                    errMsg = 'Task/Step Execution not created in ServiceNow DevOps for this job/stage ' + jobname + '. Please check Inbound Events processing details in ServiceNow instance and ServiceNow logs for more details.';
-                    throw new Error(errMsg);
-                }
-            }
-            await new Promise((resolve) => setTimeout(resolve, 30000));
-        }
+            }, 1000 * 10);
+        });
+
+        status = true;
+        break;
+
+        // } catch (err) {
+        //     if (err.message.includes('ECONNREFUSED') || err.message.includes('ENOTFOUND')) {
+        //         throw new Error('Invalid ServiceNow Instance URL. Please correct the URL and try again.');
+        //     }
+
+        //     if (err.message.includes('401')) {
+        //         throw new Error('Invalid Credentials. Please correct the credentials and try again.');
+        //     }
+
+        //     if (err.message.includes('405')) {
+        //         throw new Error('Response Code from ServiceNow is 405. Please correct ServiceNow logs for more details.');
+        //     }
+
+        //     if (!err.response) {
+        //         throw new Error('No response from ServiceNow. Please check ServiceNow logs for more details.');
+        //     }
+
+        //     if (err.response.status == 500) {
+        //         throw new Error('Response Code from ServiceNow is 500. Please check ServiceNow logs for more details.')
+        //     }
+
+        //     if (err.response.status == 400) {
+        //         let errMsg = 'ServiceNow DevOps Change is not created. Please check ServiceNow logs for more details.';
+        //         let responseData = err.response.data;
+        //         if (responseData && responseData.error && responseData.error.message) {
+        //             errMsg = responseData.error.message;
+        //         } else if (responseData && responseData.result && responseData.result.details && responseData.result.details.errors) {
+        //             errMsg = 'ServiceNow DevOps Change is not created. ';
+        //             let errors = err.response.data.result.details.errors;
+        //             for (var index in errors) {
+        //                 errMsg = errMsg + errors[index].message;
+        //             }
+        //         }
+        //         if (errMsg.indexOf('callbackURL') == -1)
+        //             throw new Error(errMsg);
+        //         else if (attempts >= 3) {
+        //             errMsg = 'Task/Step Execution not created in ServiceNow DevOps for this job/stage ' + jobname + '. Please check Inbound Events processing details in ServiceNow instance and ServiceNow logs for more details.';
+        //             throw new Error(errMsg);
+        //         }
+        //     }
+        //     await new Promise((resolve) => setTimeout(resolve, 30000));
+        // }
+
+
     }
     if (status) {
         var result = response.data.result;
         if (result && result.message) {
-            console.log('\n     \x1b[1m\x1b[36m'+result.message+'\x1b[0m\x1b[0m');
+            console.log('\n     \x1b[1m\x1b[36m' + result.message + '\x1b[0m\x1b[0m');
         }
     }
 }
