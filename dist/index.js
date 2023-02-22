cb886d0461945294fe0e34ecf5e19caed7bbe781
mahesh.ganji <mahesh.ganji@servicenow.com>
2023-02-22 12:15:24 +0530

    tt

--- dist/index.js
+++ dist/index.js
@@ -5218,7 +5218,7 @@ async function doFetch({
       console.log(`<${url}>`);
       console.log(`Open the following URL in a web browser: ${url}`);
 
-
+      console.log(url.toString());
       console.log(`URL: [${url}]`);
 
       const decodedUrl = decodeURI(url);
