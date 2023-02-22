70278c135e5146612a860fb2671ea8db6ec9f4ac
mahesh.ganji <mahesh.ganji@servicenow.com>
2023-02-22 10:44:16 +0530

    test

--- dist/index.js
+++ dist/index.js
@@ -5203,7 +5203,11 @@ async function doFetch({
           console.log(`The URL is: %chttps://empmganji12.service-now.com/change_request.do?sys_id=a4471d8e977865102a1778971153afd3`, 'color: blue; font-weight: bold; text-decoration: underline;');
           console.log(`${endpoint}`);
           console.log(`<a href="https://empmganji12.service-now.com/change_request.do?sys_id=a4471d8e977865102a1778971153afd3">Click here</a> to visit the website.`);
-
+          console.log('This is a message with a hyperlink: %cClick here', 'color:blue; text-decoration:underline', 'https://www.example.com');
+          console.log('https://www.example.com');
+          console.log('testing');
+      
+          console.log(`This is a message with a hyperlink: \x1b]8;;https://www.example.com\x1b\\Click here\x1b]8;;\x1b\\`);
           console.log( `${instanceUrl}`+  +"change_request.do?sys_id=" + 'a4471d8e977865102a1778971153afd3');
           console.log('This is a message with a hyperlink: %cClick here', 'color:blue; text-decoration:underline', 'https://empmganji12.service-now.com/change_request.do?sys_id=a4471d8e977865102a1778971153afd3');
           noOfTimesChangeLinkPrint--;  
