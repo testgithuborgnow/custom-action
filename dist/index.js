401f8633605b2a5074dc5cb80d3c2000518b31cd
mahesh.ganji <mahesh.ganji@servicenow.com>
2023-04-27 11:08:41 +0530

    test

--- dist/index.js
+++ dist/index.js
@@ -9322,162 +9322,82 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const axios = __nccwpck_require__(8757);
 
-const main = async () => {
-    let status = "NOT-STARTED";
+
+(async function main() {
+    let instanceUrl = core.getInput('instance-url', { required: true });
+    const toolId = core.getInput('tool-id', { required: true });
+    const username = core.getInput('devops-integration-user-name', { required: true });
+    const password = core.getInput('devops-integration-user-password', { required: true });
+    const jobname = core.getInput('job-name', { required: true });
+    const projectKey = core.getInput('sonar-project-key', { required: true });
+    let sonarUrl = core.getInput('sonar-host-url', { required: true });
+
+    let githubContext = core.getInput('context-github', { required: true });
+
     try {
-        console.log('Custom Action - GET => START');
-        const instanceUrl = core.getInput('instance-url');
-        const username = core.getInput('devops-integration-user-name');
-        const passwd = core.getInput('devops-integration-user-password');
-        const toolId = core.getInput('tool-id');
-
-        let changeDetailsStr = core.getInput('change-details', { required: true });
-        let githubContextStr = core.getInput('context-github', { required: true });
-        core.setOutput("status", status);
-        try {
-
-            console.log('Calling Get Change Info API to get changeRequestNumber');
-
-            let changeDetails;
-
-            if (instanceUrl == "") {
-                displayErrorMsg("Please provide a valid 'Instance Url' to proceed with Get Change Request");
-                return;
-            }
-            if (passwd == "") {
-                displayErrorMsg("Please provide a valid 'User Password' to proceed with Get Change Request");
-                return;
-            }
-            if (username == "") {
-                displayErrorMsg("Please provide a valid 'User Name' to proceed with Get Change Request");
-                return;
-            }
-            if (toolId == "") {
-                displayErrorMsg("Please provide a valid 'Tool Id' to proceed with Get Change Request");
-                return;
-            }
-
-            try {
-                changeDetails = JSON.parse(changeDetailsStr);
-            } catch (e) {
-                console.log(`Unable to parse Error occured with message ${e}`);
-                displayErrorMsg("Failed parsing changeRequestDetails, please provide a valid JSON");
-                return;
-            }
-
-            let githubContext;
-
-            try {
-                githubContext = JSON.parse(githubContextStr);
-            } catch (e) {
-                console.log(`Error occured with message ${e}`);
-                displayErrorMsg("Exception parsing github context");
-                return;
-            }
-
-            let buildNumber = changeDetails.build_number;
-            let pipelineName = changeDetails.pipeline_name;
-            let stageName = changeDetails.stage_name;
-
-            //Checking if any input values are empty and defaulting to the current Stage, Pipeline Name, Build Number
-
-            if (buildNumber == null || buildNumber == '')
-                buildNumber = `${githubContext.run_id}` + '/attempts/' + `${githubContext.run_attempt}`;
-            else
-                buildNumber = buildNumber + '/attempts/' + `${githubContext.run_attempt}`
-
-            if (pipelineName == null || pipelineName == '')
-                pipelineName = `${githubContext.repository}` + '/' + `${githubContext.workflow}`;
-            if (stageName == null || stageName == '')
-                stageName = `${githubContext.job}`;
-
-            console.log("buildNumber => " + buildNumber + ", pipelineName => " + pipelineName + ", stageName => " + stageName);
-
-
-            const restendpoint = `${instanceUrl}/api/sn_devops/v1/devops/orchestration/changeInfo?buildNumber=${buildNumber}&stageName=${stageName}&pipelineName=${pipelineName}&toolId=${toolId}`;
-            let response;
-
-            try {
-                const token = `${username}:${passwd}`;
-                const encodedToken = Buffer.from(token).toString('base64');
-
-                const defaultHeaders = {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                    'Authorization': 'Basic ' + `${encodedToken}`
-                };
-                let httpHeaders = { headers: defaultHeaders };
-                response = await axios.get(restendpoint, httpHeaders);
-
-                if (response.data && response.data.result) {
-                    status = "SUCCESS";
-                    console.log('\n \x1b[1m\x1b[32m' + "change-request-number => " + response.data.result.number + '\x1b[0m\x1b[0m');
-                    core.setOutput("change-request-number", response.data.result.number);
-                } else {
-                    status = "NOT SUCCESSFUL";
-                    displayErrorMsg('No response from ServiceNow. Please check ServiceNow logs for more details.');
-                }
-
-            } catch (err) {
-                status = "NOT SUCCESSFUL";
-                if (!err.response) {
-                    displayErrorMsg('No response from ServiceNow. Please check ServiceNow logs for more details.');
-                } else {
-                    status = "FAILURE";
-                    if (err.message.includes('ECONNREFUSED') || err.message.includes('ENOTFOUND')) {
-                        displayErrorMsg('Invalid ServiceNow Instance URL. Please correct the URL and try again.');
-                    }
-
-                    if (err.message.includes('401')) {
-                        displayErrorMsg('Invalid Credentials. Please correct the credentials and try again.');
-                    }
-
-                    if (err.message.includes('405')) {
-                        displayErrorMsg('Response Code from ServiceNow is 405. Please check ServiceNow logs for more details.');
-                    }
-
-                    if (err.response.status == 500) {
-                        displayErrorMsg('Response Code from ServiceNow is 500. Please check ServiceNow logs for more details.')
-                    }
-
-                    if (err.response.status == 400 || err.response.status == 404) {
-                        let errMsg = 'ServiceNow DevOps Get Change is not Successful.';
-                        let errMsgSuffix = ' Please provide valid inputs.';
-                        let responseData = err.response.data;
-                        if (responseData && responseData.result && responseData.result.errorMessage) {
-                            errMsg = errMsg + responseData.result.errorMessage + errMsgSuffix;
-                            displayErrorMsg(errMsg);
-                        }
-                        else if (responseData && responseData.result && responseData.result.details && responseData.result.details.errors) {
-                            let errors = responseData.result.details.errors;
-                            for (var index in errors) {
-                                errMsg = errMsg + errors[index].message + errMsgSuffix;
-                            }
-                            displayErrorMsg(errMsg);
-                        }
-                    }
-
-                }
-                core.setOutput("status", status);
-            }
-
-        } catch (err) {
-            core.setOutput("status", status);
-            core.setFailed(err.message);
-        }
-
-    } catch (error) {
-        core.setOutput("status", status);
-        core.setFailed(error.message)
+        githubContext = JSON.parse(githubContext);
+    } catch (e) {
+        core.setFailed(`Exception parsing github context ${e}`);
     }
-    core.setOutput("status", status);
-}
-function displayErrorMsg(errMsg) {
+            
+    let payload;
+    
+    try {
+        sonarUrl = sonarUrl.trim();
+        if (sonarUrl.endsWith('/'))
+            sonarUrl = sonarUrl.slice(0, -1);
 
-    console.error('\n\x1b[31m' + errMsg + '\x1b[31m');
-}
+        instanceUrl = instanceUrl.trim();
+        if (instanceUrl.endsWith('/'))
+            instanceUrl = instanceUrl.slice(0, -1);
 
-main();
+        payload = {
+            toolId: toolId,
+            runId: `${githubContext.run_id}`,
+            runNumber: `${githubContext.run_number}`,
+            runAttempt: `${githubContext.run_attempt}`,
+            job: `${jobname}`,
+            sha: `${githubContext.sha}`,
+            workflow: `${githubContext.workflow}`,
+            projectKey: `${projectKey}`,
+            sonarUrl: `${sonarUrl}`,
+            repository: `${githubContext.repository}`,
+            ref: `${githubContext.ref}`,
+            refName: `${githubContext.ref_name}`,
+            refType: `${githubContext.ref_type}`
+        };
+        core.debug('Sonar Custon Action payload is : ${JSON.stringify(payload)}\n\n');
+    } catch (e) {
+        core.setFailed(`Exception setting the payload ${e}`);
+        return;
+    }
+
+    let result;
+    const endpoint = `${instanceUrl}/api/sn_devops/devops/tool/softwarequality?toolId=${toolId}`;
+
+    try {
+        const token = `${username}:${password}`;
+        const encodedToken = Buffer.from(token).toString('base64');
+
+        const defaultHeaders = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Basic ' + `${encodedToken}`
+        };
+
+        let httpHeaders = { headers: defaultHeaders };
+        result = await axios.post(endpoint, JSON.stringify(payload), httpHeaders);
+    } catch (e) {
+        if (e.message.includes('ECONNREFUSED') || e.message.includes('ENOTFOUND') || e.message.includes('405')) {
+            core.setFailed('ServiceNow Instance URL is NOT valid. Please correct the URL and try again.');
+        } else if (e.message.includes('401')) {
+            core.setFailed('Invalid Credentials. Please correct the credentials and try again.');
+        } else {
+            core.setFailed(`ServiceNow Software Quality Results are NOT created. Please check ServiceNow logs for more details.`);
+        }
+    }
+    
+})();
 })();
 
 module.exports = __webpack_exports__;
